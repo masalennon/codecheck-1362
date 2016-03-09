4547996606a966d3907e5d9c487208e34de6b9d6
Masanari Miyazaki <competence.80@gmail.com>
2016-03-09 09:59:34 +0900

    ss

--- app/hello.js
+++ app/hello.js
@@ -3,9 +3,9 @@ function hello(name) {
 * ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
-  
+
   //ここにHelloworldを実装します。
-  return "Hello World!";
+  return "Hello "+name+"!";
 }
 
 module.exports = hello;
